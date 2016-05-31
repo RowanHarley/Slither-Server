ea8e663f30a49bac210052a32cbcece5fb6dac51
Rowan Harley <rowanharley01@gmail.com>
2016-05-31 15:09:29 +0100

    Create config.js

---
+++ Config/config.js
@@ -0,0 +1,8 @@
+var Settings = {
+    port: 8080,
+    bots: 0, // Not Implemented
+    maxConnections: 100,
+    startFood: 20000,
+    foodPerSpawn: 1000, // Not Implemented
+    gameRadius: 21600
+}
