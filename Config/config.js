0c61a997d7b1ac9b24e3928460a2aeb089a0a597
Rowan Harley <rowanharley01@gmail.com>
2016-05-31 16:38:57 +0100

    Delete config.js

--- Config/config.js
+++
@@ -1,9 +0,0 @@
-var Settings = {
-    client = "slither.io",
-    port: 8080,
-    bots: 0, // Not Implemented
-    maxConnections: 100,
-    startFood: 20000,
-    foodPerSpawn: 1000, // Not Implemented
-    gameRadius: 21600
-}
