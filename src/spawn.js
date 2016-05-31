b146bc90f786fbabc0f8e03e5930b367a3f1eb05
Rowan Harley <rowanharley01@gmail.com>
2016-05-31 21:50:20 +0100

    Update spawn.js

--- src/spawn.js
+++ src/spawn.js
@@ -1,6 +1,13 @@
 var config = require("../config/config.js");
+var cell = require("./Cell.js");
+var player = require("./Player.js");
 
 function startSpawnFood(){
 }
-function spawnFoodSector(sectorID){
+function spawnFoodSector(){
+    var SectorID = player.Sector; // Get Player Sector
+    
+}
+function SpawnFood(){
+    
 }
