51b1c03849798f33432c9a33e8315436b4f498ef
Rowan Harley <rowanharley01@gmail.com>
2016-06-01 09:46:07 +0100

    Update config.js
    - Added 2 new variables
    - changed var to module.exports

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
-var Settings = {
+module.exports = {
     client = "slither.io",
     port: 8080,
     bots: 0, // Not Implemented
     maxConnections: 100,
     startFood: 20000,
     foodPerSpawn: 1000, // Not Implemented
-    gameRadius: 21600
+    gameRadius: 21600,
+    sectorSize: 480
 }
