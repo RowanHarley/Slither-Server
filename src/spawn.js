7fe47915704a13a1ce915dc80e16e39304872d4c
Rowan Harley <rowanharley01@gmail.com>
2016-06-03 10:57:59 +0100

    fixed broken file link

--- src/spawn.js
+++ src/spawn.js
@@ -1,6 +1,6 @@
 var config = require("../config/config.js");
 var cell = require("./Cell.js");
-var player = require("./Player.js");
+var player = require("./entities/Player.js");
 
 function startSpawnFood(){
 }
