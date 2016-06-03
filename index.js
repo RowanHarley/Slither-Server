7a4b351dff51a3b7da5846995d8b9ec7a4df0c55
Rowan Harley <rowanharley01@gmail.com>
2016-06-03 12:51:25 +0100

    Fixed Server not connecting

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var config = require("./config/config.js");
 var spawn = require("./src/spawn.js");
-var WebSocket = require("ws");
+var WebSocketServer = require("ws").Server;
 
 console.log("[SERVER] Starting Server...");
-var Conn = new WebSocket("ws://" + config["client"] + "/" + config["port"]);
+var server;
+server = new WebSocketServer({port: config["port"], path: "/slither"});
 
-console.log("[SERVER] Server Started at " + Conn + "! Waiting for Connections...");
+console.log("[SERVER] Server Started at " + server + "! Waiting for Connections...");
+server.on('error', function() {
+        console.log('npm ws error');
+});
 
+server.on('open', function HandleConnections(conn){
+	ws.send("test");
+});
 
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
+({
+  close: function() {
+    return this.server.close();
+  }
+});
